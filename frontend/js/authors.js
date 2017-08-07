d5dcb3cc13a26a571fa807b45bfa699b88553ead
Joanna Trojak <joannatrojak19@gmail.com>
2017-08-07 11:15:36 +0200

    function addauthor to dom

--- frontend/js/authors.js
+++ frontend/js/authors.js
@@ -1,3 +1,16 @@
+function authorAdd(singleAuthor)
+{
+    var newAuthor = '<li class="list-group-item">' +
+                    '<div class="panel panel-default">'+
+                    '<div class="panel-heading"><span class="authorTitle">'+singleAuthor.name+' '+singleAuthor.surname+'</span>'+
+                    '<button data-id="'+singleAuthor.id+'" class="btn btn-danger pull-right btn-xs btn-author-remove">'+
+                    'class="fa fa-trash"></i></button>'+
+                    '</div>'+
+                    '</div>'+
+                    '</li>-->'; 
+    var list = document.querySelector('authorsList');
+    list.innerHTML += newAuthor; 
+}
 document.addEventListener('DOMContentLoaded', function()
 {
     var authorAdd = document.querySelector('#authorAdd'); 
